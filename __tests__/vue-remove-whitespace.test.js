9b918ee84b425c5c672e87c190c27f43ecbdc1eb
aokiken <aokiken@me.com>
2021-03-31 03:38:56 +0900

    fix

--- __tests__/vue-remove-whitespace.test.js
+++ __tests__/vue-remove-whitespace.test.js
@@ -7,7 +7,7 @@ describe('InlineElementWithLineBreak', () => {
   it('renders the correct markup', () => {
     expect(wrapper.html()).toBe('<div><span>1</span><span>2</span><span>3</span><span>4</span><span>5</span></div>')
   })
-  it.only('Cant bind event after removing whitespace', done => {
+  it('Cant bind event after removing whitespace', done => {
     const wrapper = mount(InlineElementWithLineBreakAndBindEvent)
     wrapper.find('[data-test="item0"]').trigger('click')
     wrapper.vm.$nextTick(() => {
